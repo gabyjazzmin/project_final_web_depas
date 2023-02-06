5552b567ce17932d6c877b4a25afe7c7556759ec
gabyjazzmin <gabrielaj.4.f@gmail.com>
2023-02-05 18:43:50 -0600

    add colors to the map

--- js/scriptMaps.js
+++ js/scriptMaps.js
@@ -1,1 +1,11 @@
-﻿
+﻿let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+    mapId:'19d44d248a609478'
+  });
+}
+
+window.initMap = initMap;
