5bdb055f5a428cb25e7facab7f597b71eaa9f0d4
gabyjazzmin <gabrielaj.4.f@gmail.com>
2023-02-05 22:09:14 -0600

    corrección de ubicación de departamentos

--- js/scriptMaps.js
+++ js/scriptMaps.js
@@ -2,10 +2,11 @@
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 25.65310258792058, lng: -100.29139610922921 },
+    zoom: 18,
     mapId:'19d44d248a609478'
   });
 }
+// Info de resis 25.65310258792058, -100.29139610922921
 
 window.initMap = initMap;
